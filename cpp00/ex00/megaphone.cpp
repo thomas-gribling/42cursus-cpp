7de71e6af5024f2f0e5434c21482db5278dbfc34
thomas-gribling <tgriblin@student.42perpignan.fr>
2024-04-23 15:41:44 +0200

    ex00 working

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
-int	main() {
-	std::cout << "Hello World!\n";
+int	main(int ac, char **av) {
+	std::string	curr;
+
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else {
+		for (int i = 1; i < ac; i++) {
+			curr.assign(av[i]);
+			for (int j = 0; j < curr.length(); j++)
+				std::cout << (char)toupper(curr[j]);
+		}
+	}
+	std::cout << '\n';
 	return 0;
 }
