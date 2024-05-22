5f8c472449bd8452ce1df8c85ef1fc35e62b4cad
thomas-gribling <tgriblin@student.42perpignan.fr>
2024-05-22 15:31:45 +0200

    progress on cpp02

---
+++ cpp02/ex02/Fixed.hpp
@@ -0,0 +1,34 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <cmath>
+
+class Fixed {
+	public:
+		int	value;
+		static int const	fractBits = 8;
+	public:
+		Fixed();
+		Fixed(Fixed const &source);
+		Fixed(int const int_n);
+		Fixed(float const float_n);
+		~Fixed();
+
+		Fixed &operator=(Fixed const &source);
+		bool operator>(Fixed const &source);
+		bool operator<(Fixed const &source);
+		bool operator>=(Fixed const &source);
+		bool operator<=(Fixed const &source);
+		bool operator==(Fixed const &source);
+		bool operator!=(Fixed const &source);
+
+		int	getRawBits( void ) const;
+		void	setRawBits( int const raw );
+		float	toFloat( void ) const;
+		int	toInt( void ) const;
+};
+
+std::ostream& operator<<(std::ostream& os, const Fixed& f);
+
+#endif
