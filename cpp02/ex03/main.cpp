3b11d9d501a87c706c9538d68302bde13e12b72d
thomas-gribling <tgriblin@student.42perpignan.fr>
2024-05-27 10:14:25 +0200

    beautified

--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -10,11 +10,14 @@ void	testPoint( Point const &a, Point const &b, Point const &c, Point const &p )
 int	main () {
 	Point const a(0, 10);
 	Point const b(-5, 0);
-	Point const c(5, 0);
+	Point const c(5,  0);
 
-	testPoint(a, b, c, Point(3, 3)); // dedans
+	testPoint(a, b, c, Point(3,  3)); // dedans
 	testPoint(a, b, c, Point(10, 0));
 	testPoint(a, b, c, Point(-3, 5));
-	testPoint(a, b, c, Point(6, 9));
-	testPoint(a, b, c, Point(1, 1)); // dedans
+	testPoint(a, b, c, Point(6,  9));
+	testPoint(a, b, c, Point(1,  1)); // dedans
+	testPoint(a, b, c, Point(0, 10));
+
+	return 0;
 }
