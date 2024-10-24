c7a754c309f9635862be0414db4d276d25b70661
thomas-gribling <tgriblin@student.42perpignan.fr>
2024-10-24 08:32:00 +0200

    added test for const

--- cpp_module_07/ex01/main.cpp
+++ cpp_module_07/ex01/main.cpp
@@ -4,6 +4,10 @@ void square(int &n) {
 	n *= n;
 }
 
+void display(double const &d) {
+	std::cout << d << std::endl;
+}
+
 int main() {
 	int array[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int len = 10;
@@ -20,6 +24,14 @@ int main() {
 	for (int i = 0; i < len - 1; i++) {
 		std::cout << array[i] << ", ";
 	}
-	std::cout << array[len - 1] << std::endl << std::endl;
+	std::cout << array[len - 1] << std::endl;
+
+
+	double darray[] = {0.1, 1.5, 3.4, 6.6};
+	len = 4;
+
+	std::cout << std::endl << "DISPLAY TEST:" << std::endl;
+	iter(darray, len, display);
+	std::cout << std::endl;
 	return 0;
 }
